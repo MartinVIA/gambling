f47f9355bf475ee68fe1509d05a3829661175fde
GuillermoUNEA <guillermo.chavez@alumnos.uneatlantico.es>
2025-11-14 09:20:02 +0100

    dev 2

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # gambling
+
+Card logic [done]
+player [Victor]
+{
+    buttons{
+        title card
+        while game
+    }
+    controls{
+        keyboard function
+    }
+}
+processing & house AI [Martin]
+Title card
+assets
+
--- script.js
+++ script.js
@@ -4,15 +4,18 @@ var myCards=[
 [[2,false],[3,false],[4,false],[5,false],[6,false],[7,false],[8,false],[9,false],[10,false],['J',false],['Q',false],['K',false],['A',false]],
 [[2,false],[3,false],[4,false],[5,false],[6,false],[7,false],[8,false],[9,false],[10,false],['J',false],['Q',false],['K',false],['A',false]]
 ]
+// An array of cards, [type[number,has been picked or not]]
 
 function generate(){
     var cardType=["Hearts","Cloves","Spades","Diamonds"]
+    // this determined what card type the card is. 
     var randomType=Math.floor((Math.random()*myCards.length));
     var randomNumber=Math.floor((Math.random()*myCards[randomType].length));
     var generatedcard="images/"+cardType[randomType]+".png";
+    // non modular idea to parce the card type to a url
     
     document.getElementById("cardImage").src=generatedcard;
     document.getElementById("card").innerHTML="Card is "+myCards[randomType][randomNumber][0]+" of "+cardType[randomType]+" and "+myCards[randomType][randomNumber][1];
-    document.getElementsByClassName("number")[0].innerText="b"+myCards[randomType][randomNumber][0];
+    document.getElementsByClassName("number")[0].innerText=""+myCards[randomType][randomNumber][0];
     myCards[randomType][randomNumber][1]=true;
 }
