eff8d914c58bcf9adf03fa5c129c4fa274309038
GuillermoUNEA <guillermo.chavez@alumnos.uneatlantico.es>
2025-11-14 10:16:24 +0100

    revamp

--- script.js
+++ script.js
@@ -1,38 +1,52 @@
-//var myCards=[
-//[[2,false],[3,false],[4,false],[5,false],[6,false],[7,false],[8,false],[9,false],[10,false],['J',false],['Q',false],['K',false],['A',false]],
-//[[2,false],[3,false],[4,false],[5,false],[6,false],[7,false],[8,false],[9,false],[10,false],['J',false],['Q',false],['K',false],['A',false]],
-//[[2,false],[3,false],[4,false],[5,false],[6,false],[7,false],[8,false],[9,false],[10,false],['J',false],['Q',false],['K',false],['A',false]],
-//[[2,false],[3,false],[4,false],[5,false],[6,false],[7,false],[8,false],[9,false],[10,false],['J',false],['Q',false],['K',false],['A',false]]
-//]
-// An array of cards, [type[number,has been picked or not]]
-
-//function generate(){
-  //  var cardType=["Hearts","Cloves","Spades","Diamonds"]
-    // this determined what card type the card is. 
-    //var randomType=Math.floor((Math.random()*myCards.length));
-    //var randomNumber=Math.floor((Math.random()*myCards[randomType].length));
-    //var generatedcard="images/"+cardType[randomType]+".png";
-    // non modular idea to parce the card type to a hyperlink image
-    
-    //document.getElementById("cardImage").src=generatedcard;
-    //document.getElementById("card").innerHTML="Card is "+myCards[randomType][randomNumber][0]+" of "+cardType[randomType]+" and "+myCards[randomType][randomNumber][1];
-    //document.getElementsByClassName("number")[0].innerText=""+myCards[randomType][randomNumber][0];
-    //myCards[randomType][randomNumber][1]=true;
-//}
-
+// Simple card deck and generator
+const suits = ['hearts', 'diamonds', 'clubs', 'spades'];
+const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace'];
 const deck = [];
 
-const suits = ['hearts', 'diamonds', 'clubs', 'spades'];
-const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace']
-
+// build the deck
 suits.forEach(suit => {
     values.forEach(value => {
         deck.push({
             suit: suit,
             value: value,
-            imageUrl: `images/${value}_of_${suit}.png`,
-            code: `${value.charAt(0)}${suit.charAt(0)}`,
+            // filename convention: images/<value>_of_<suit>.png
+            imageUrl:"images/2_of_hearts.png",
+            code: `${value.charAt(0).toUpperCase()}${suit.charAt(0).toUpperCase()}`
+        });
+    });
+});
 
-        })
-    })
-})
+// generate a random card and update the page
+function generate(){
+    if (!deck.length) {
+        document.getElementById('card').textContent = 'No cards in deck';
+        return null;
+    }
+
+    const randomIndex = Math.floor(Math.random() * deck.length);
+    const selectedCard = deck[randomIndex];
+
+    // update the card text
+    document.getElementById('card').textContent = `Card is ${selectedCard.value} of ${selectedCard.suit}`;
+
+    // compute display for the big card number/letter
+    let display = selectedCard.value;
+    switch (selectedCard.value) {
+        case 'jack': display = 'J'; break;
+        case 'queen': display = 'Q'; break;
+        case 'king': display = 'K'; break;
+        case 'ace': display = 'A'; break;
+        default: display = selectedCard.value; // keeps '10' as '10'
+    }
+
+    const numEl = document.querySelector('.number');
+    if (numEl) numEl.innerText = display;
+
+    // update image (if file exists with that name)
+    const img = document.getElementById('cardImage');
+    if (img) img.src = selectedCard.imageUrl;
+
+    return selectedCard;
+}
+
+document.getElementById
